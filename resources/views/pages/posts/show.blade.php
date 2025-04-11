28f4eef3649bf3eb7ef270af37588163fb0f1693
nickramsay19 <nickramsay15@gmail.com>
2025-04-11 19:41:49 +1000

    renders markdown text

--- resources/views/pages/posts/show.blade.php
+++ resources/views/pages/posts/show.blade.php
@@ -1,6 +1,6 @@
 <x-layout title="{{ $post ? $post->title : '404 Post not found' }}">
     @if ($post)
-        <div class="flex flex-row">
+        <div class="flex flex-row mb-1">
             <div class="basis-full grow">
                 <span>{{ $post->created_at_date }}</span>
                 @foreach ($post->tags as $tag)
@@ -11,7 +11,7 @@
             <x-posts.command-menu :post="$post" class="justify-end" />
         </div>
 
-        <div style="margin-top: 1em;">{!! $post->body !!}</div>
+        <div class="post-body">{!! Illuminate\Mail\Markdown::parse($post->body) !!}</div>
     @else
         There is no post with by this name. Go <x-link to="home">home</x-link>?
     @endif
