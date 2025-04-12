d43d1f95a99b5e1933c428c237c42b0d16910266
nickramsay19 <nickramsay15@gmail.com>
2025-04-12 12:39:12 +1000

    remove boot debug comment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,6 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function boot(): void {
         if (config('app.env') === 'production') {
-            \Log::info('yo!');
             URL::forceScheme('https');
         }
     }
