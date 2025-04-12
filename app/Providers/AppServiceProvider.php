d287d4399a82501a949d4310f748a86a9862d527
nickramsay19 <nickramsay15@gmail.com>
2025-04-12 12:38:44 +1000

    force https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
  
 class AppServiceProvider extends ServiceProvider {
     /**
@@ -16,6 +17,9 @@ class AppServiceProvider extends ServiceProvider {
      * Bootstrap any application services.
      */
     public function boot(): void {
-
+        if (config('app.env') === 'production') {
+            \Log::info('yo!');
+            URL::forceScheme('https');
+        }
     }
 }
