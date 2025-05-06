f5c29e73617c21ca32626994dbc21d45e815ea70
nickramsay19 <nickramsay15@gmail.com>
2025-05-06 19:27:31 +1000

    made index page capital letters for consistency

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,10 +1,10 @@
-<x-layout title="index">
+<x-layout title="Index">
     <section class="mt-2">
         
         <x-posts.list :posts="$posts" readonly />
 
         <hr class="text-dark-gamma mt-4 mb-2" />
-        
+
         <x-link to="posts" class="font-mono font-sembold underline">see all posts</x-link>
     </section>
 </x-layout>
